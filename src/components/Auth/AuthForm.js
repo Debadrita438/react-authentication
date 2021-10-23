f37a1b0602311c62f7fc503c41bba625e82a970f
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-23 10:57:18 +0530

    Applying login and signup using google firebase and using context for state change

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,28 +1,92 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+
+import ENV from '../../env';
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
     const [ isLogin, setIsLogin ] = useState(true);
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ isLoading, setIsLoading ] = useState(false);
+    const authCtx = useContext(AuthContext);
 
     const switchAuthModeHandler = () => {
         setIsLogin((prevState) => !prevState);
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        let url;
+
+        if (isLogin) {
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${ENV.firebaseApiKey}`;
+        } else {
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ENV.firebaseApiKey}`;
+        }
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password,
+                returnSecureToken: true
+            })
+        })
+            .then((res) => {
+                setIsLoading(false);
+                if (res.ok) {
+                    setEmail('');
+                    setPassword('');
+                    return res.json();
+                } else {
+                    return res.json().then((data) => {
+                        let errorMessage = 'Authentication failed!';
+                        // if (data && data.error && data.error.message) {
+                        //     errorMessage = data.error.message;
+                        // }
+                        throw new Error(errorMessage);
+                    });
+                }
+            })
+            .then((data) => {
+                authCtx.login(data.idToken);
+            })
+            .catch((err) => alert(err.message));
+    };
+
     return (
         <section className={classes.auth}>
             <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor='email'>Your Email</label>
-                    <input type='email' id='email' required />
+                    <input
+                        type='email'
+                        id='email'
+                        required
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='password'>Your Password</label>
-                    <input type='password' id='password' required />
+                    <input
+                        type='password'
+                        id='password'
+                        required
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
                 </div>
                 <div className={classes.actions}>
-                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                    {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+                    {isLoading && <p>Sending Request...</p>}
                     <button type='button' className={classes.toggle} onClick={switchAuthModeHandler}>
                         {isLogin ? 'Create new account' : 'Login with existing account'}
                     </button>
---
+++ src/env.js
@@ -0,0 +1,5 @@
+const vars = {
+    firebaseApiKey: 'AIzaSyClVEZe5sWKOg0mtH3z4YCU5dC7FBs6ZsA'
+};
+
+export default vars;
